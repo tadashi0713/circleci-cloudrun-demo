cb0c9b3c09c83affd9b85a338b3e18d0c03f5a9a
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-09-13 10:52:21 +0900

    Remove cypress

--- cypress/integration/index.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Index', () => {
-    it('Should show text after click button', () => {
-        cy.visit('/')
-        cy.get('[data-testid="Button"]').click()
-        cy.get('[data-testid="Text"]').should('have.text', 'Hello')
-    })
-})
