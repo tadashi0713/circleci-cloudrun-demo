9d9a8d62c828b29df1e30aca9f66c95cac0c9a07
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-01-20 19:24:56 +0900

    Change text

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="flex h-screen">
       <div className="m-auto">
         <button data-testid="Button" onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white text-4xl font-bold py-4 px-8 rounded">Click</button>
-        { showText ? <p data-testid="Text" className="text-4xl pt-10 text-center">Hello</p> : null }
+        { showText ? <p data-testid="Text" className="text-4xl pt-10 text-center">CircleCI</p> : null }
       </div>
     </div>
   );
